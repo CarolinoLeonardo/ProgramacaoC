e2ec7dc8bb565537a6510809833c3750458ce8b1
Leonardo Carolino <35158506+CarolinoLeonardo@users.noreply.github.com>
2026-06-11 18:12:28 -0300

    Add files via upload

---
+++ Pratica_durante_aulas/DeclarandoVariaves.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int idade; //Declara uma variavel inteira chamada "idade"
+    int quantidade; //Declara uma variavel inteira chamada "quantidade"
+    float altura; //Declara uma variavel de ponto flutuante chamada "altura"
+    char letra; //Declara uma variavel de caractere chamada "letra"
+    char nome[20] = "Alice"; //Declara um array de 20 caracteres e inicializa com "Alice"
+    double salario; //Declara uma variavel de ponto flutuante de dupla precisão chamada "salario"
+
+    idade = 34;
+    
+    return 0;
+
+}
